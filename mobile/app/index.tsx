61151b7526fc604d6a647ef4c669896a257d8763
Sergio100807 <sgrf@academico.ufpb.br>
2026-04-17 11:50:06 -0300

    feat: adicona lógica básica de cronometro

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { Audio } from 'expo-av';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Index() {
   const [permissionResponse, requestPermission] = Audio.usePermissions();
   const [isRecording, setIsRecording] = useState(false);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     async function askForPermission() {
@@ -17,20 +18,39 @@ export default function Index() {
     askForPermission();
   }, []);
 
+  useEffect(() => {
+    let intervalo;
+
+    if (isRecording) {
+      intervalo = setInterval(() => {
+        setSeconds((numeroAntigo) => numeroAntigo + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalo);
+      setSeconds(0);
+    }
+
+    return () => clearInterval(intervalo);
+  }, [isRecording]);
+
+  // Função pro botão iniciar a gravação
   const handleRecordPress = () => {
     if (!isRecording) {
       setIsRecording(true);
     }
   };
 
+  // Função pro botão parar
   const handleStopPress = () => {
     if (isRecording) {
       setIsRecording(false);
     }
   };
-
   return (
     <View style={styles.container}>
+      <Text style={styles.timerText}>
+        {seconds}
+      </Text>
       <View style={styles.controlsContainer}>
         <TouchableOpacity
           style={[styles.recordButton, isRecording && styles.recordButtonActive]}
@@ -60,6 +80,14 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+  timerText: {
+    fontSize: 56, // Letra gigante pra dar destaque
+    fontWeight: '300', // Deixa a letra mais fina
+    color: '#333333',
+    marginBottom: 40, // Um espacinho pra baixo
+    fontFamily: 'monospace',
+    fontVariant: ['tabular-nums'], // Faz os números ficarem todos do mesmo tamanho
+  },
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
