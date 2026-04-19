588e3b755877716ffbf5a4fbeca34cd75b695e4c
Sergio100807 <sgrf@academico.ufpb.br>
2026-04-18 21:57:52 -0300

    chore(backend): autorização de rede via CORS para testes mobile

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.speech import router as speech_router
 
 app = FastAPI(
     title="Destravar API",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(speech_router)
